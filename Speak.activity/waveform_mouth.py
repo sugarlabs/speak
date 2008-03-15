7db7514dbca45c4531b037d7da772888d441527a
Joshua Minor <j@lux.vu>
2008-03-15 12:30:21 -0700

    Cleaned up headers a bit.

--- Speak.activity/waveform_mouth.py
+++ Speak.activity/waveform_mouth.py
@@ -1,5 +1,6 @@
 # Speak.activity
 # A simple front end to the espeak text-to-speech engine on the XO laptop
+# http://wiki.laptop.org/go/Speak
 #
 # Copyright (C) 2008  Joshua Minor
 # This file is part of Speak.activity
@@ -12,13 +13,13 @@
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
 # 
-#     Foobar is distributed in the hope that it will be useful,
+#     Speak.activity is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 # 
 #     You should have received a copy of the GNU General Public License
-#     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#     along with Speak.activity.  If not, see <http://www.gnu.org/licenses/>.
 
 # This code is a stripped down version of the waveform view from Measure
 
