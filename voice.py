24f512c802ae5e954200a3c87cb7044850a9d8ee
Rafael Ortiz <rafael@activitycentral.com>
2012-07-22 15:52:13 -0500

    change in voices names SL#3773

--- voice.py
+++ voice.py
@@ -36,7 +36,7 @@ import espeak
 # then they won't get translated, but they
 # should still show up in the interface.
 expectedVoiceNames = [
-    _("Brazil"),
+    _("Portuguese (Brazil)"),
     _("Swedish"),
     _("Icelandic"),
     _("Romanian"),
@@ -56,11 +56,11 @@ expectedVoiceNames = [
     _("Cantonese"),
     _("Scottish"),
     _("Greek"),
-    _("Vietnam"),
+    _("Vietnamese"),
     _("English"),
     _("Lancashire"),
     _("Italian"),
-    _("Portugal"),
+    _("Portuguese"),
     _("German"),
     _("Whisper"),
     _("Croatian"),
