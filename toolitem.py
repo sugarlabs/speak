4593e2bbe5a4afad853b4cc403a46675c199a0f5
Rafael Ortiz <rafael@activitycentral.com>
2012-07-27 18:17:55 -0500

    some style fixes

--- toolitem.py
+++ toolitem.py
@@ -28,8 +28,10 @@ class ToolWidget(Gtk.ToolItem):
         self.label = Gtk.Label(label_text)
         self._box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         
-        if self.label: self._box.pack_start(self.label, True, True, 5)
-        if self.wid: self._box.pack_start(self.wid, True, True, 5)
+        if self.label:
+                self._box.pack_start(self.label, True, True, 5)
+        if self.wid:
+                self._box.pack_start(self.wid, True, True, 5)
         
         self.add(self._box)
         self.show_all()
