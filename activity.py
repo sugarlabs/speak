sugarlabs/speak

commit c3a1b59f3edd3b0debe91de645a0c5025aaa88d1

Add a use my face option

activity.py
@@ -50,7 +50,9 @@ from sugar.graphics.combobox import ComboBox
 from sugar.graphics.toolbarbox import ToolbarBox, ToolbarButton
 from sugar.activity.widgets import ActivityToolbarButton
 from sugar.activity.widgets import StopButton
+from sugar.graphics.objectchooser import ObjectChooser
 
+from sugar import mime
 from sugar import profile
 
 import eye
@@ -66,10 +68,13 @@ import fft_mouth
 import waveform_mouth
 
 import face
+import photoface
 
 import voice as voice_model
 import brain
 import chat
+
+from faceselect import FaceSelector
 
 import espeak
 
@@ -83,6 +88,8 @@ ACCELEROMETER_DEVICE = '/sys/devices/platform/lis3lv02d/position'
 MODE_TYPE = 1
 MODE_BOT = 2
 MODE_CHAT = 3
+FACE_CARTOON = 1
+FACE_PHOTO = 2
 MOUTHS = [mouth.Mouth, fft_mouth.FFTMouth, waveform_mouth.WaveformMouth]
 NUMBERS = ['one', 'two', 'three', 'four', 'five']
 SLEEPY_EYES = sleepy.Sleepy
@@ -158,61 +165,63 @@ class SpeakActivity(activity.Activity):
         self._active_eyes = None
         self._active_number_of_eyes = None
         self._current_voice = None
+        self._face_type = FACE_CARTOON
 
         # make an audio device for playing back and rendering audio
         self.connect('notify::active', self._active_cb)
         self._cfg = {}
 
         # make a box to type into
-        hbox = gtk.HBox()
+        self._entry_box = gtk.HBox()
 
         if self._tablet_mode:
             self._entry = gtk.Entry()
-            hbox.pack_start(self._entry, expand=True)
+            self._entry_box.pack_start(self._entry, expand=True)
             talk_button = ToolButton('microphone')
             talk_button.set_tooltip(_('Speak'))
             talk_button.connect('clicked', self._talk_cb)
-            hbox.pack_end(talk_button, expand=False)
+            self._entry_box.pack_end(talk_button, expand=False)
         else:
             self._entrycombo = gtk.combo_box_entry_new_text()
             self._entrycombo.connect('changed', self._combo_changed_cb)
             self._entry = self._entrycombo.child
             self._entry.set_size_request(-1, style.GRID_CELL_SIZE)
-            hbox.pack_start(self._entrycombo, expand=True)
+            self._entry_box.pack_start(self._entrycombo, expand=True)
         self._entry.set_editable(True)
         self._entry.connect('activate', self._entry_activate_cb)
         self._entry.connect('key-press-event', self._entry_key_press_cb)
         self._entry.modify_font(pango.FontDescription(str='sans bold 24'))
-        hbox.show()
+        self._entry_box.show()
 
         self.face = face.View(fill_color=lighter)
+        self._cartoon_face = self.face
         self.face.set_size_request(
             -1, gtk.gdk.screen_height() - 2 * style.GRID_CELL_SIZE)
         self.face.show()
 
         # layout the screen
-        box = gtk.VBox(homogeneous=False)
+        self._box = gtk.VBox(homogeneous=False)
         if self._tablet_mode:
-            box.pack_start(hbox, expand=False)
-            box.pack_start(self.face)
+            self._box.pack_start(self._entry_box, expand=False)
+            self._box.pack_start(self.face)
         else:
-            box.pack_start(self.face, expand=True)
-            box.pack_start(hbox)
+            self._box.pack_start(self.face, expand=True)
+            self._box.pack_start(self._entry_box)
 
         self.add_events(gtk.gdk.POINTER_MOTION_HINT_MASK
                         | gtk.gdk.POINTER_MOTION_MASK)
         self.connect('motion_notify_event', self._mouse_moved_cb)
 
-        box.add_events(gtk.gdk.BUTTON_PRESS_MASK)
-        box.connect('button_press_event', self._mouse_clicked_cb)
+        self._box.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+        self._box.connect('button_press_event', self._mouse_clicked_cb)
 
         # desktop
         self._notebook.show()
         self._notebook.props.show_border = False
         self._notebook.props.show_tabs = False
 
-        box.show_all()
-        self._notebook.append_page(box)
+        self._box.show_all()
+        self._notebook.append_page(self._box)
 
         self._chat = chat.View()
         self._chat.show_all()
@@ -231,22 +240,22 @@ class SpeakActivity(activity.Activity):
 
         toolbox.toolbar.insert(self._activity_button, -1)
 
-        mode_type = RadioToolButton(
+        self._mode_type = RadioToolButton(
             named_icon='mode-type',
             tooltip=_('Type something to hear it'))
-        mode_type.connect('toggled', self.__toggled_mode_type_cb)
-        toolbox.toolbar.insert(mode_type, -1)
+        self._mode_type.connect('toggled', self.__toggled_mode_type_cb)
+        toolbox.toolbar.insert(self._mode_type, -1)
 
         mode_robot = RadioToolButton(
             named_icon='mode-robot',
-            group=mode_type,
+            group=self._mode_type,
             tooltip=_('Ask robot any question'))
         mode_robot.connect('toggled', self.__toggled_mode_robot_cb)
         toolbox.toolbar.insert(mode_robot, -1)
 
         self._mode_chat = RadioToolButton(
             named_icon='mode-chat',
-            group=mode_type,
+            group=self._mode_type,
             tooltip=_('Voice chat'))
         self._mode_chat.connect('toggled', self.__toggled_mode_chat_cb)
         toolbox.toolbar.insert(self._mode_chat, -1)
@@ -362,8 +371,21 @@ class SpeakActivity(activity.Activity):
 
         current_voice = self.face.status.voice
 
-        status = self.face.status = \
-            face.Status().deserialize(self._cfg['status'])
+        type_ = self._cfg['face_type']
+        lighter = style.Color(self._colors[_lighter_color(self._colors)])
+        if type_ == self._face_type:
+            status = self.face.status = \
+                face.Status().deserialize(self._cfg['status'])
+        elif type_ == FACE_CARTOON:
+            self._set_face(face.View(fill_color=lighter), FACE_CARTOON)
+            self._cartoon_face = self.face
+            status = self.face.status = \
+                face.Status().deserialize(self._cfg['status'])
+        else:
+            status = photoface.Status().deserialize(self._cfg['status'])
+            view = photoface.View(*status.get_args(), fill_color=lighter)
+            status = view.status
+            self._set_face(view, FACE_PHOTO)
 
         found_my_voice = False
         for name in self._voice_evboxes.keys():
@@ -382,16 +404,17 @@ class SpeakActivity(activity.Activity):
         self.pitchadj.value = self.face.status.pitch
         self.rateadj.value = self.face.status.rate
 
-        if status.mouth in MOUTHS:
-            self._mouth_type[MOUTHS.index(status.mouth)].set_active(True)
+        if self._face_type == FACE_CARTOON:
+            if status.mouth in MOUTHS:
+                self._mouth_type[MOUTHS.index(status.mouth)].set_active(True)
 
-        self._number_of_eyes_changed_event_cb(
-            None, None, NUMBERS[len(status.eyes) - 1], True)
-        for name in EYE_DICT.keys():
-            if status.eyes[0] == EYE_DICT[name]['widget']:
-                self._eye_type[name].set_icon(name + '-selected')
-                self._eyes_changed_event_cb(None, None, name, True)
-                break
+            self._number_of_eyes_changed_event_cb(
+                None, None, NUMBERS[len(status.eyes) - 1], True)
+            for name in EYE_DICT.keys():
+                if status.eyes[0] == EYE_DICT[name]['widget']:
+                    self._eye_type[name].set_icon(name + '-selected')
+                    self._eyes_changed_event_cb(None, None, name, True)
+                    break
 
         self._entry.props.text = self._cfg['text'].encode('utf-8', 'ignore')
         if not self._tablet_mode:
@@ -410,6 +433,7 @@ class SpeakActivity(activity.Activity):
             history = [unicode(i[0], 'utf-8', 'ignore')
                        for i in self._entrycombo.get_model()]
         cfg = {'status': self.face.status.serialize(),
+               'face_type': self._face_type,
                'text': unicode(self._entry.props.text, 'utf-8', 'ignore'),
                'history': history, }
         file(file_path, 'w').write(json.dumps(cfg))
@@ -595,6 +619,24 @@ class SpeakActivity(activity.Activity):
     def _make_face_bar(self):
         facebar = gtk.Toolbar()
 
+        self._photo_face = ToolButton('face')
+        self._photo_face.set_tooltip(_('Set face from photo'))
+        self._photo_face.connect('clicked', self._photo_face_cb)
+        facebar.insert(self._photo_face, -1)
+        self._photo_face.show()
+
+        self._clear = ToolButton('edit-delete')
+        self._clear.set_tooltip(_('Clear photo face'))
+        self._clear.set_sensitive(False)
+        self._clear.connect('clicked', self._clear_photo_cb)
+        facebar.insert(self._clear, -1)
+        self._clear.show()
+
+        separator = gtk.SeparatorToolItem()
+        separator.set_draw(True)
+        separator.set_expand(False)
+        facebar.insert(separator, -1)
+
         self._mouth_type = []
         self._mouth_type.append(RadioToolButton(
             named_icon='mouth',
@@ -681,8 +723,79 @@ class SpeakActivity(activity.Activity):
         facebar.insert(number_of_eyes_palette_button, -1)
         number_of_eyes_palette_button.show()
 
+        self._cartoon_face_buttons = self._mouth_type + \
+            [eye_palette_button, number_of_eyes_palette_button]
+
         facebar.show_all()
         return facebar
+
+    def _photo_face_cb(self, widget):
+        chooser = ObjectChooser(parent=self,
+                                what_filter=mime.GENERIC_TYPE_IMAGE)
+
+        result = chooser.run()
+        if result == gtk.RESPONSE_ACCEPT:
+            jobject = chooser.get_selected_object()
+            if jobject and jobject.file_path:
+                selector = FaceSelector(jobject.file_path)
+                selector.connect('face-processed',
+                                 self._photo_face_processed_cb)
+                selector.connect('cancel', self._photo_face_cancel_cb)
+                self._notebook.append_page(selector)
+                selector.show()
+
+                num = self._notebook.page_num(selector)
+                self._notebook.set_current_page(num)
+        chooser.destroy()
+                
+    def _photo_face_processed_cb(self, widget, *face_data):
+        lighter = style.Color(self._colors[_lighter_color(self._colors)])
+        self._set_face(photoface.View(*face_data, fill_color=lighter),
+                       FACE_PHOTO)
+
+    def _photo_face_cancel_cb(self, widget):
+        self._notebook.set_current_page(0)
+
+    def _set_face(self, view, type_):
+        self._face_type = type_
+        cartoon = type_ == FACE_CARTOON
+
+        self.face.shut_up()
+        self._box.remove(self.face)
+        self._box.remove(self._entry_box)
+
+        self.face = view
+        self.face.set_size_request(
+            -1, gtk.gdk.screen_height() - 2 * style.GRID_CELL_SIZE)
+
+        if self._tablet_mode:
+            self._box.pack_start(self._entry_box, expand=False)
+            self._box.pack_start(self.face)
+        else:
+            self._box.pack_start(self.face, expand=True)
+            self._box.pack_start(self._entry_box)
+        self.face.show()
+
+        if not cartoon and self._mode == MODE_CHAT:
+            self._mode = MODE_TYPE
+            self._mode_type.set_active(True)
+            self._mode_chat.set_active(False)
+
+            self._chat.shut_up()
+            self._voice_palette.set_content(self._voice_box)
+            self._set_voice()
+        self._notebook.set_current_page(0)
+
+        self._photo_face.set_sensitive(cartoon)
+        self._clear.set_sensitive(not cartoon)
+        for bnt in self._cartoon_face_buttons:
+            bnt.set_sensitive(cartoon)
+        self._mode_chat.set_sensitive(cartoon)
+
+
+    def _clear_photo_cb(self, widget):
+        lighter = style.Color(self._colors[_lighter_color(self._colors)])
+        self._set_face(self._cartoon_face, FACE_CARTOON)
 
     def _face_palette_cb(self, button):
         palette = button.get_palette()
@@ -698,6 +811,9 @@ class SpeakActivity(activity.Activity):
                 return MOUTHS[i]
 
     def _mouth_changed_cb(self, ignored, quiet):
+        if self._face_type == FACE_PHOTO:
+            return
+
         value = self._get_active_mouth()
         if value is None:
             return
@@ -737,6 +853,9 @@ class SpeakActivity(activity.Activity):
         return None
 
     def _eyes_changed_event_cb(self, widget, event, name, quiet):
+        if self._face_type == FACE_PHOTO:
+            return
+
         if self._active_eyes is not None:
             for old_name in EYE_DICT.keys():
                 if EYE_DICT[old_name]['index'] == self._active_eyes:
@@ -756,6 +875,9 @@ class SpeakActivity(activity.Activity):
                 self.face.say_notification(_('eyes changed'))
 
     def _number_of_eyes_changed_event_cb(self, widget, event, name, quiet):
+        if self._face_type == FACE_PHOTO:
+            return
+
         if self._active_number_of_eyes is not None:
             old_name = NUMBERS[self._active_number_of_eyes - 1]
             self._number_of_eyes_type[old_name].set_icon(old_name)
@@ -856,6 +978,8 @@ class SpeakActivity(activity.Activity):
                                                   self._set_idle_phrase)
 
     def _load_sleeping_face(self):
+        if self._face_type == FACE_PHOTO:
+            return
         current_eyes = self.face.status.eyes
         self.face.status.eyes = [SLEEPY_EYES] * self._active_number_of_eyes
         self._update_face()
@@ -966,6 +1090,10 @@ class SpeakActivity(activity.Activity):
     def _remove_idle(self):
         if self._robot_idle_id is not None:
             gobject.source_remove(self._robot_idle_id)
+
+            if self._face_type == FACE_PHOTO:
+                return
+
             value = self._get_active_eyes()
             if value is not None:
                 self.face.status.eyes = [value] * self._active_number_of_eyes

photoface.py
@@ -1,0 +1,298 @@
+# Speak.activity
+# A simple front end to the espeak text-to-speech engine on the XO laptop
+# http://wiki.laptop.org/go/Speak
+#
+# Copyright (C) 2014 Sam Parkinson
+# This file is part of Speak.activity
+# 
+#     Speak.activity is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Speak.activity is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Speak.activity.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import json
+import math
+import uuid
+import base64
+import logging 
+import cStringIO
+from struct import unpack
+
+import gtk
+import numpy.core
+import sugar.graphics.style as style
+
+import voice
+import espeak
+from faceselect import Eye
+from faceselect import Mouth
+from face import remove_curses
+
+logger = logging.getLogger('speak')
+
+
+_EYE_CIRCUMFERENCE = 3
+_BALL_DIST_CIRC_RATIO = 27
+
+
+def _b64_to_pixbuf(b64):
+    data = base64.b64decode(b64)
+    path = '/tmp/{}.jpeg'.format(uuid.uuid4())
+
+    with open(path, 'wb') as f:
+        f.write(data)
+    pixbuf = gtk.gdk.pixbuf_new_from_file(path)
+
+    os.remove(path)
+    return pixbuf
+
+
+class Status(object):
+
+    def __init__(self):
+        self.voice = voice.defaultVoice()
+        self.pitch = espeak.PITCH_MAX / 2
+        self.rate = espeak.RATE_MAX / 2
+
+    def serialize(self):
+        fake_file = cStringIO.StringIO()
+        self.pixbuf.save_to_callback(fake_file.write, 'jpeg')
+        pixbuf_b64 = base64.b64encode(fake_file.getvalue())
+
+        fake_file = cStringIO.StringIO()
+        self.mouth.pixbuf.save_to_callback(fake_file.write, 'jpeg')
+        mouth_pixbuf_b64 = base64.b64encode(fake_file.getvalue())
+
+        mouth_dict = self.mouth.__dict__
+        mouth_dict['pixbuf'] = mouth_pixbuf_b64
+
+        return json.dumps({
+            'voice': {'language': self.voice.language,
+                        'name': self.voice.name},
+            'pitch': self.pitch,
+            'rate': self.rate,
+            'left_eye': self.left_eye.__dict__,
+            'right_eye': self.right_eye.__dict__,
+            'mouth': mouth_dict,
+            'pixbuf': pixbuf_b64})
+
+    def deserialize(self, buf):
+        data = json.loads(buf)
+
+
+        self.voice = voice.Voice(data['voice']['language'],
+                data['voice']['name'])
+        self.pitch = data['pitch']
+        self.rate = data['rate']
+
+        self.left_eye = Eye(tuple(data['left_eye']['center']),
+                            data['left_eye']['circ'])
+        self.right_eye = Eye(tuple(data['right_eye']['center']),
+                             data['right_eye']['circ'])
+        self.pixbuf = _b64_to_pixbuf(data['pixbuf'])
+
+        m = data['mouth']
+        mouth_pixbuf = _b64_to_pixbuf(m['pixbuf'])
+        self.mouth = Mouth()
+        self.mouth.from_values(m['x'], m['y'], m['w'], m['h'], mouth_pixbuf)
+
+        return self
+
+    def clone(self):
+        new = Status()
+        new.voice = self.voice
+        new.pitch = self.pitch
+        new.rate = self.rate
+        new.pixbuf = self.pixbuf
+        new.left_eye = self.left_eye
+        new.right_eye = self.right_eye
+        new.mouth = self.mouth
+        return new
+
+    def get_args(self):
+        return [self.pixbuf, self.left_eye, self.right_eye, self.mouth]
+
+
+class View(gtk.DrawingArea):
+    def __init__(self, pixbuf, left_eye, right_eye, mouth,
+                 fill_color=style.COLOR_BUTTON_GREY):
+        gtk.DrawingArea.__init__(self)
+
+
+        self.status = Status()
+        self._color = fill_color
+        self.status.left_eye = left_eye
+        self.status.right_eye = right_eye
+        self.status.mouth = mouth
+        self.status.pixbuf = pixbuf
+
+        self._buffers = []
+        self._buffer_size = 256
+        self._main_buffers = []
+        self._newest_buffer = []
+        self._volume = 0
+        self._look_x = None
+        self._look_y = None
+
+        self._audio = espeak.AudioGrab()
+        self._audio.connect('new-buffer', self.__new_buffer_cb)
+        self._pending = None
+
+        self.connect('expose-event', self.__draw_cb)
+
+    def _redraw(self):
+        alloc = self.get_allocation()
+        self.queue_draw_area(0, 0, alloc.width, alloc.height)
+
+    def __draw_cb(self, widget, cr):
+        cr = widget.window.cairo_create()
+        alloc = widget.get_allocation()
+
+        offset_x = (alloc.width - self.status.pixbuf.get_width()) / 2
+        offset_y = (alloc.height - self.status.pixbuf.get_height()) / 2
+
+        # Background Color
+        cr.rectangle(0, 0, alloc.width, alloc.height)
+        cr.set_source_color(self._color.get_gdk_color())
+        cr.fill()
+
+        # Face Pixbuf
+        cr.rectangle(offset_x, offset_y, alloc.width, alloc.height)
+        cr.set_source_pixbuf(self.status.pixbuf, offset_x, offset_y)
+        cr.fill()
+
+        # Mouth
+        self._process_buffer()
+        volume = min(self._volume / 30000.0, 1.0)
+
+        # Draw a background for when the mouth moves
+        cr.rectangle(offset_x + self.status.mouth.x,
+                     offset_y + self.status.mouth.y,
+                     self.status.mouth.w,
+                     self.status.mouth.h)
+        cr.set_source_color(self._color.get_gdk_color())
+        cr.fill()
+
+        volume_offset = 100.0 * volume
+        cr.rectangle(offset_x + self.status.mouth.x,
+                     offset_y + self.status.mouth.y + volume_offset,
+                     self.status.mouth.w,
+                     self.status.mouth.h)
+        cr.set_source_pixbuf(self.status.mouth.pixbuf,
+                             offset_x + self.status.mouth.x,
+                             offset_y + self.status.mouth.y + volume_offset)
+        cr.fill()
+
+        # Eye centers
+        for eye in (self.status.left_eye, self.status.right_eye):
+            cr.arc(eye.center[0] + offset_x, eye.center[1] + offset_y,
+                   eye.circ, 0, 2 * math.pi)
+            cr.set_source_rgb(1.0, 1.0, 1.0)
+            cr.fill()
+
+            if self._look_x is None or self._look_y is None:
+                look_x = eye.center[0] + offset_x + alloc.x
+                look_y = eye.center[1] + offset_y + alloc.y
+                x, y, circ = self._compute_pupil(eye, offset_x, offset_y,
+                                                 look_x, look_y)
+            else:
+                x, y, circ = self._compute_pupil(eye, offset_x, offset_y,
+                                                 self._look_x, self._look_y)
+            cr.arc(x - alloc.x, y - alloc.y, circ, 0, 2 * math.pi)
+            cr.set_source_rgb(0.0, 0.0, 0.0)
+            cr.fill()
+
+    # Thanks to xeyes :)
+    def _compute_pupil(self, eye, offset_x, offset_y, look_x, look_y):
+        CIRC = eye.circ / _EYE_CIRCUMFERENCE
+        EYE_X, EYE_Y = self.translate_coordinates(
+                self.get_toplevel(),
+                int(eye.center[0] + offset_x),
+                int(eye.center[1] + offset_y))
+        EYE_HWIDTH = CIRC
+        EYE_HHEIGHT = CIRC
+        BALL_DIST = EYE_HWIDTH / (eye.circ / _BALL_DIST_CIRC_RATIO * 4)
+
+        dx = look_x - EYE_X
+        dy = look_y - EYE_Y
+
+        if dx or dy:
+            angle = math.atan2(dy, dx)
+            cosa = math.cos(angle)
+            sina = math.sin(angle)
+            h = math.hypot(EYE_HHEIGHT * cosa, EYE_HWIDTH * sina)
+            x = (EYE_HWIDTH * EYE_HHEIGHT) * cosa / h
+            y = (EYE_HWIDTH * EYE_HHEIGHT) * sina / h
+            dist = BALL_DIST * math.hypot(x, y)
+
+            if dist < math.hypot(dx, dy):
+                dx = dist * cosa
+                dy = dist * sina
+
+        return dx + EYE_X, dy + EYE_Y, CIRC
+
+    def _process_buffer(self):
+        if len(self._main_buffers) == 0 or len(self._newest_buffer) == 0:
+            self._volume = 0
+        else:
+            self._volume = numpy.core.max(self._main_buffers)
+
+    def __new_buffer_cb(self, obj, buf):
+        if len(buf) < 28:
+            self._newest_buffer = []
+        else:
+            self._newest_buffer = list(
+                unpack(str(int(len(buf)) / 2) + 'h', buf))
+            self._main_buffers += self._newest_buffer
+            if(len(self._main_buffers) > self._buffer_size):
+                del self._main_buffers[0:(len(self._main_buffers) - \
+                        self._buffer_size)]
+
+        self._redraw()
+        return True
+
+    def set_border_state(self, state):
+        pass
+
+    def look_ahead(self):
+        self._look_x = None
+        self._look_y = None
+        self._redraw()
+
+    def look_at(self, pos=None):
+        if pos is None:
+            display = gtk.gdk.display_get_default()
+            screen, self._look_x, self._look_y, mods = display.get_pointer()
+        else:
+            self._look_x, self._look_y = pos
+        self._redraw()
+        
+
+    def update(self, status=None):
+        pass
+
+    def set_voice(self, voice):
+        self.status.voice = voice
+        self.say_notification(voice.friendlyname)
+
+    def say(self, something):
+        curse_free = remove_curses(something)
+        self._audio.speak(self._pending or self.status, curse_free)
+
+    def say_notification(self, something):
+        status = (self._pending or self.status).clone()
+        status.voice = voice.defaultVoice()
+        self._audio.speak(status, something)
+
+    def shut_up(self):
+        self._audio.stop_sound_device()

commit 84297ed0ed1e85f3dd7021e6a5f5386f2194243e

Fix a flake8 F841 local assigned not used
F841 local variable 'lighter' is assigned to but never used

Regression introduced by c3a1b59f3edd3b0debe91de645a0c5025aaa88d1.

activity.py
@@ -806,9 +806,7 @@ class SpeakActivity(activity.Activity):
             bnt.set_sensitive(cartoon)
         self._mode_chat.set_sensitive(cartoon)
 
-
     def _clear_photo_cb(self, widget):
-        lighter = style.Color(self._colors[_lighter_color(self._colors)])
         self._set_face(self._cartoon_face, FACE_CARTOON)
 
     def _face_palette_cb(self, button):